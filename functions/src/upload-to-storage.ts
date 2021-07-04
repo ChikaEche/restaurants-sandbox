f7b5d5c9984a9e5af322c8ed984d630f507cd4e2
ChikaEche <chikaokafor94@gmail.com>
2021-07-04 22:14:55 +0100

    wip

--- functions/src/upload-to-storage.ts
+++
@@ -1,9 +0,0 @@
-import { storage } from './config';
-
-export const uploadImageToStorage = async (src: string) => {
-  const storageRef = storage.bucket();
-
-  const response = await storageRef.upload(src, {destination: 'restaurants/images/'});
-  const url = await response[0].getSignedUrl({action: 'read', expires: 0})
-
-}
